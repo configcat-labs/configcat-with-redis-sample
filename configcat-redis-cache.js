fc2363ff3cd8afbb88375723bb5f0778620239df
Chavez Harris <codedbychavez@gmail>
2023-03-14 08:15:33 -0300

    Added logic in configcat-redis-cache.js and index.js

--- configcat-redis-cache.js
+++ configcat-redis-cache.js
@@ -1,14 +1,55 @@
 const util = require("util");
 const redis = require("redis");
 
-// Add the configcatRedisCache function below this line.
+function configcatRedisCache(redisClientOptions) {
 
+  this.isRedisAvailable = false;
+  this.cacheClient = redis.createClient(redisClientOptions);
 
+  this.cacheClient.connect();
 
-// Add the .get method here below this line.
+  this.cacheClient.on("connect", () => {
+    console.log("Connected to Redis");
+    this.isRedisAvailable = true;
+  });
 
+  this.cacheClient.on("error", (error) => {
+    console.log(`Redis error: ${error}`);
+    this.isRedisAvailable = false;
+  });
 
+  this.lastCacheItems = {};
+}
 
-// Add the .set method below this line.
+configcatRedisCache.prototype.get = async function (key) {
+  if (!this.isRedisAvailable) {
+    return this.lastCacheItems[key];
+  }
+
+  try {
+    const getAsync = util
+      .promisify(this.cacheClient.get)
+      .bind(this.cacheClient);
+
+    return JSON.parse(await getAsync(key));
+  } catch (error) {
+    console.error(`Cache read failed! \n ${error}`);
+    return this.lastCacheItems[key];
+  }
+};
+
+configcatRedisCache.prototype.set = async function (key, item) {
+  this.lastCacheItems[key] = item;
+
+  try {
+    const setAsync = util
+      .promisify(this.cacheClient.set)
+      .bind(this.cacheClient);
+
+    await setAsync(key, JSON.stringify(item));
+  } catch (error) {
+    console.error(`Cache write failed! \n ${error}`);
+  }
+};
 
 module.exports = configcatRedisCache;
--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const configcat = require("configcat-node");
 const configcatRedisCache = require("./configcat-redis-cache");
+
+const redisOptions = { host: "localhost", port: 6379 };
+
+const configCatClient = configcat.getClient("0yDbCLmNK0qIUakB2LFJDA/u0Z5j4oDjkuHKOVJkIo9Dw", configcat.PollingMode.AutoPoll, 
+  {
+    cache: new configcatRedisCache(redisOptions)
+  }
+);
+
+setInterval(() => {
+  configCatClient.getValueAsync("isMyAwesomeFeatureEnabled", false).then(value => {
+    console.log(`${new Date().toTimeString()} isMyAwesomeFeatureEnabled: ${value}`);
+  });
+}, 5000);
